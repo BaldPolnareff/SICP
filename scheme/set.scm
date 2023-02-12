4fc9bed6948f21787d182263398d0f9cbc998662
Giorgio <jeanpierrepolnareff@protonmail.com>
2023-02-12 20:47:44 +0100

    started chapter 3 although I admit I rushed the last parts of chapter 2 out of frustration, oops

---
+++ scheme/set.scm
@@ -0,0 +1,31 @@
+(define balance 100)
+
+(define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount)) balance)
+        "Insufficient funds"))
+
+(define new-withdraw
+    (let ((balance 100))
+        (lambda (amount) (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Insufficient funds"))))
+
+(define (make-withdraw balance)
+    (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount)) balance)
+        "Porcoddio Mahmood Soldi")))
+
+(define (make-account balance)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Porcoddio Mahmood Soldi"))
+    (define (deposit amount)
+        (set! balance (+ balance amount)) balance)
+    (define (dispatch m)
+        (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request - MAKE-ACCOUNT: " m))))
+    dispatch)
