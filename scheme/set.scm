bd5d2b9d32d849921ca83c4a2dbaf6b5f8888a9b
Giorgio <jeanpierrepolnareff@protonmail.com>
2023-02-14 01:29:49 +0100

    doing chapter 3 exercises

--- scheme/set.scm
+++ scheme/set.scm
@@ -29,3 +29,46 @@
               ((eq? m 'deposit) deposit)
               (else (error "Unknown request - MAKE-ACCOUNT: " m))))
     dispatch)
+
+; SICP Exercise 3.1
+
+(define (make-accumulator initial)
+    (let ((accumulator initial))
+        (lambda (x) (set! accumulator (+ x accumulator)) accumulator)))
+
+; SICP Exercise 3.2
+
+(define (make-monitored fun)
+    (let ((counter 0))
+        (define (dispatch mf)
+            (cond ((equal? mf 'how-many-calls?) counter)
+                  ((equal? mf 'reset-count) (set! counter 0))
+                  (else (begin (set! counter (inc counter)) (fun mf)))))
+    dispatch))
+
+; SICP Exercise 3.3
+
+(define (make-account balance password)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Porcoddio Mahmood Soldi"))
+    (define (deposit amount)
+        (set! balance (+ balance amount)) balance)
+    (let ((count 0))
+        (define (dispatch pass m)
+            (if (equal? pass password)
+                (cond ((equal? m 'withdraw) withdraw)
+                      ((equal? m 'deposit) deposit)
+                      (else (error "Unknown operation - MAKE-ACCOUNT: " m)))
+                (begin (set! count (+ count 1))
+                       (when (> count 7)
+                            (call-the-cops))
+                       (error "Too many attempts (out of 7), remaining: " (- 7 count))))))
+    dispatch)
+
+(define (call-the-cops)
+    (error "Nino nino porcoddioooooooo"))
+
+(define (increment n)
+    ((make-accumulator n) 1))
